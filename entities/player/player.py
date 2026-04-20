0a56e1ebf1fddb46d347e2aca0334b46eb68f195
Nikita Bychkov <sandcave2017@gmail.com>
2026-04-20 23:19:41 +0500

    Создал базовый player класс

---
+++ entities/player/player.py
@@ -0,0 +1,22 @@
+import pygame
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("entities\player\player.png").convert_alpha()
+        self.rect = self.image.get_rect(midbottom = (640, 360))
+
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= 10
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += 10
+        if keys[pygame.K_UP]:
+            self.rect.y -= 10
+        if keys[pygame.K_DOWN]:
+            self.rect.y += 10
+
+    def update(self):
+        self.player_input()
