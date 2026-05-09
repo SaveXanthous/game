0839a4d57a159591711366b3b3a3856eafecf35e
SaveXanthous <savexanthous@gmail.com>
2026-05-09 13:20:26 +0500

    добавлен новый manager ui_manager.py также ui_manager.py b ui_scaler.py

--- core/managers/ui_manager.py
+++ core/managers/ui_manager.py
@@ -1,77 +1,49 @@
-import pygame
 import pygame_gui
-from pip._internal import resolution
 
-from core.game_handle import GameHandle
+from game.game_handle import GameHandle
+from ui.ui_scaler import UIScaler
+from utils.dir_handler import DirHandler
+from core.ui.ui_compiler import UICompiler
 from utils.json_handler import JSONHandler
+
 
 class UIManager(GameHandle):
     def __init__(self, game_manager):
         GameHandle.__init__(self, game_manager)
 
         resolution = game_manager.settings_manager.graphics_settings.resolution
-
         self._manager = pygame_gui.UIManager(resolution)
-        self.ui_elements = {}
 
+        self._ui_elements = {}
+
+        self.ui_compiler = UICompiler(game_manager, self._manager)
+        self.ui_scaler = UIScaler(game_manager)
     @property
     def manager(self):
         return self._manager
+
+    @property
+    def ui_elements(self):
+        return self._ui_elements
 
     @manager.setter
     def manager(self, value):
         self._manager = value
 
+    @ui_elements.setter
+    def ui_elements(self, value):
+        self._ui_elements = value
+
     def kill(self):
-        for key_ui_element in self.ui_elements:
-            self.ui_elements[key_ui_element].kill()
+        for key_ui_element in self._ui_elements:
+            self._ui_elements[key_ui_element].kill()
 
-    def load_ui_from_json(self, file_path):
-        data = JSONHandler.read(file_path)
+    def set_ui_json(self, path):
+        self._ui_elements = self.ui_compiler.get_ui_json(path)
 
-        width = self.game_manager.settings_manager.graphics_settings.width
-        self.ui_elements = {}
-
-        for item in data['elements']:
-            rect = self._get_ui_rect(item, width)
-            ui_element = None
-
-            if item['type'] == "UILabel":
-                ui_element = self._add_ui_label(item, rect)
-            elif item['type'] == "UIButton":
-                ui_element = self._add_ui_button(item, rect)
-
-            if ui_element is not None:
-                self.ui_elements[item['id']] = ui_element
-                print(item['id'])
-
-        return self.ui_elements
-
-    def _get_ui_rect(self, item,width):
-        rect_dictanory= item['rect']
-
-        rx = rect_dictanory["x"]
-        ry = rect_dictanory["y"]
-        rh = rect_dictanory["h"]
-        rw = rect_dictanory["w"]
-
-        rw = width if rw == -1 else rw
-
-        rect = pygame.Rect((rx, ry), (rw, rh))
-        return rect
-
-    def _add_ui_label(self, item, rect):
-        return pygame_gui.elements.UILabel(
-            relative_rect=rect,
-            text=item['text'],
-            manager=self._manager,
-            anchors=item['anchors']
-        )
-
-    def _add_ui_button(self, item, rect):
-        return pygame_gui.elements.UIButton(
-            relative_rect=rect,
-            text=item['text'],
-            manager=self._manager,
-            anchors=item['anchors']
-        )
+    def update_ui(self):
+        path = JSONHandler.path_join('scenes')
+        for dir in DirHandler.get_dirs(path):
+            self.ui_scaler.scaled_ui(dir)
+        self.kill()
+        self.game_manager.scenes_manager.current_scene.init_ui()
