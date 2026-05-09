7c15da9797e6c8b4d65cf81dd0e7627dc6751148
SaveXanthous <savexanthous@gmail.com>
2026-05-09 13:16:59 +0500

    events_manager.py переделана функция remove

--- core/managers/events_manager.py
+++ core/managers/events_manager.py
@@ -1,5 +1,5 @@
 from events.default_events import DefaultEvents
-from core.game_handle import GameHandle
+from game.game_handle import GameHandle
 
 class EventsManager(GameHandle):
 
@@ -7,7 +7,7 @@ class EventsManager(GameHandle):
     def __init__(self, game_manager):
         GameHandle.__init__(self, game_manager)
         self._events = []
-        self.add_event(DefaultEvents(self._game_manager))
+        self.add_events(DefaultEvents(self._game_manager))
 
     @property
     def events(self):
@@ -17,8 +17,11 @@ class EventsManager(GameHandle):
     def events(self, value):
         self._events = value
     
-    def add_event(self, event):
+    def add_events(self, event):
         self._events.append(event)
     
-    def remove_event(self, event):
-        self._events.remove(event)
+    def remove_events(self, event):
+        try:
+            self._events.remove(event)
+        except ValueError:
+            print("event did not remove")
