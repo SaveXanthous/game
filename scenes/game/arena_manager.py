cb64664c4268f05eadcd0d9691732d9f9c88db64
Nikita Bychkov <sandcave2017@gmail.com>
2026-05-17 20:48:02 +0500

    Теперь способности активируются через ability_manager

--- scenes/game/arena_manager.py
+++ scenes/game/arena_manager.py
@@ -14,25 +14,15 @@ class ArenaManager:
         self.player = player
 
         self.enemy_types = dict()
-        self.ability_types = dict()
 
     def update_enemy_types(self, new_type, duration):
         self.enemy_types.update({new_type: Timer(duration * (2 - self.difficulty))})
-
-    def update_ability_types(self, new_type, duration):
-        self.ability_types.update({new_type: Timer(duration * (2 - self.difficulty))})
 
     def spawn_enemies(self):
         for enemy_class, timer in self.enemy_types.items():
             if timer.check():
                 if enemy_class == Enemy.type:
                     Enemy(self.player, self)
-
-    def use_abilities(self):
-        for ability_class, timer in self.ability_types.items():
-            if timer.check():
-                if ability_class == Ability.type:
-                    Ability()
 
     def update_difficulty(self):
         if self.difficulty_timer.check():
@@ -46,4 +36,3 @@ class ArenaManager:
             self.update_difficulty()
 
         self.spawn_enemies()
-        self.use_abilities()
