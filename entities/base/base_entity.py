32bac0d31d4f0d5956601e97028c117458bc347b
SaveXanthous <savexanthous@gmail.com>
2026-05-17 15:37:29 +0500

    base_entity.py переделано перемещение с учетом колизей

--- entities/base/base_entity.py
+++ entities/base/base_entity.py
@@ -4,11 +4,13 @@ from pygame.math import Vector2
 from random import randint
 
 from entities.animation_component.animation import Animation
+from entities.world.world import World
 
 
 class BaseEntity(ABC, pygame.sprite.Sprite):
 
     container = pygame.sprite.Group()
+    _world = None
     _id_gen = 0
 
     def __init_subclass__(cls, **kwargs):
@@ -30,7 +32,7 @@ class BaseEntity(ABC, pygame.sprite.Sprite):
         self.image = self.animations[self.current_state].get_current_frame()
         self.rect = self.image.get_rect(midbottom = (randint(0, 1280), randint(0, 720)))
 
-        self.pos = Vector2(self.rect.x, self.rect.y)
+        self.pos = Vector2(self.rect.centerx, self.rect.centery)
         self.velocity = Vector2(0, 0)
         self.speed = 1
         self.friction = 0.15
@@ -38,12 +40,22 @@ class BaseEntity(ABC, pygame.sprite.Sprite):
         self._type = "none"
 
         self.hitbox = self.rect.inflate(-80 * 2, -80 * 2)
+        self.hitbox.bottom = self.rect.bottom
         self.hp = 1
         self.damage = 0
         self.last_hit_time = 0
         self.invincibility_duration = 500
 
         self.id = BaseEntity.generate_new_id()
+
+
+    @property
+    def world(cls):
+        return cls._world
+
+    @world.setter
+    def world(cls, value):
+        cls._world = value
 
 
     @property
@@ -98,6 +110,24 @@ class BaseEntity(ABC, pygame.sprite.Sprite):
         cls._id_gen += 1
         return cls._id_gen
 
+    def can_move_to(self, new_center):
+        temp_rect = self.hitbox.copy()
+        temp_rect.center = (new_center.x, new_center.y)
+
+        corners = [
+            temp_rect.topleft,
+            temp_rect.topright,
+            temp_rect.bottomleft,
+            temp_rect.bottomright
+        ]
+
+        for corner in corners:
+            if self.world.get_tile_at(corner[0], corner[1]) == 0:
+                return False
+        return True
+
+
+
 
     def take_damage(self, amount):
         now = pygame.time.get_ticks()
@@ -109,13 +139,20 @@ class BaseEntity(ABC, pygame.sprite.Sprite):
 
     def move(self):
         self.velocity *= (1 - self.friction)
+        if self.velocity.length() < 0.1:
+            self.velocity = Vector2(0, 0)
 
-        if self.velocity.length() < 0.1: self.velocity = Vector2(0, 0)
+        target_pos = self.pos + self.velocity * self.speed
 
-        self.pos += self.velocity * self.speed  # нужно потом добавить time_delta
+        next_center_x = Vector2(target_pos.x, self.pos.y)
+        if self.can_move_to(next_center_x):
+            self.pos.x = target_pos.x
 
-        self.rect.center = self.pos
+        next_center_y = Vector2(self.pos.x, target_pos.y)
+        if self.can_move_to(next_center_y):
+            self.pos.y = target_pos.y
 
+        self.rect.center = (int(self.pos.x), int(self.pos.y))
         self.hitbox.center = self.rect.center
 
     def kill(self):
