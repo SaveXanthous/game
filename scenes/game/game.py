05da327ef89f48796fc8437e317b8587c6d3eea3
SaveXanthous <savexanthous@gmail.com>
2026-05-17 15:36:14 +0500

    game.py

--- scenes/game/game.py
+++ scenes/game/game.py
@@ -7,12 +7,30 @@ from entities.player.player import Player
 from entities.enemy.enemy import Enemy
 from entities.base.base_entity import BaseEntity
 from entities.camera.camera import Camera
+from entities.world.world import World
 
 class Game(BaseScene):
     def __init__(self, game_manager):
         super().__init__(game_manager)
 
+        world = World()
+
+        self.world = world
+        self.world.generate_new_world()
+
+        BaseEntity.world = self.world
+
+        spawn_x = (world.map_size[0] * world.tile_size) // 2
+        spawn_y = (world.map_size[1] * world.tile_size) // 2
+
         self.player = Player()
+        self.player.pos = (spawn_x, spawn_y)
+
+
+        self.camera = Camera()
+        self.camera.set_target_camera(self.player)
+
+        # ------------------------------------------------------------------
 
         self.arena_manager = ArenaManager(self.player, self.game_manager)
 
@@ -23,16 +41,12 @@ class Game(BaseScene):
 
         self.arena_manager.update_ability_types(Ability.type, 1000)
 
-        self.camera = Camera()
-        self.camera.set_target_camera(self.player)
 
     def update(self):
         self.arena_manager.update()
-
         BaseEntity.container.update()
 
 
     def draw(self):
-        self.camera.draw(BaseEntity.container)
-        # BaseEntity.container.draw(self.screen)
-
+        self.world.render(self.game_manager.screen, self.camera.offset.x, self.camera.offset.y)
+        self.camera.draw()
