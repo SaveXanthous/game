da0b5c9430f7354eedc807c10aa3eda0c0901822
SaveXanthous <savexanthous@gmail.com>
2026-05-20 00:55:25 +0500

    обновление списков

--- entities/upgrades_list/upgrades_list.py
+++ entities/upgrades_list/upgrades_list.py
@@ -3,28 +3,32 @@ UPGRADES_LIST = [
         "id": "arrow_dmg",
         "ability_type": "arrow",
         "upgrade_type": "damage",
-        "description": "Increase arrow damage",
+        "description": "Arrow damage",
+        'image': 'data/upgrade/arrow_dmg.png',
         "max_level": 4
     },
     {
         "id": "arrow_spd",
         "ability_type": "arrow",
         "upgrade_type": "speed",
-        "description": "Increase arrow speed",
+        "description": "Arrow speed",
+        'image': 'data/upgrade/arrow_spd.png',
         "max_level": 4
     },
     {
         "id": "arrow_amt",
         "ability_type": "arrow",
         "upgrade_type": "amount",
-        "description": "Fires an additional arrow",
+        "description": "Additional arrow",
+        'image': 'data/upgrade/arrow_amt.png',
         "max_level": 2
     },
     {
         "id": "arrow_rch",
         "ability_type": "arrow",
         "upgrade_type": "recharge",
-        "description": "Decrease arrow recharge",
+        "description": "Arrow recharge",
+        'image': 'data/upgrade/arrow_rch.png',
         "max_level": 4
     }
 ]
